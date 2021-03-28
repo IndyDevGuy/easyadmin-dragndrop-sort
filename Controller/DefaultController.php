c2cd8e6fc08af5ad3d2e800a8dec75bafdb5298d
IndyDevGuy <illusiongrouppcs@gmail.com>
2021-03-28 16:01:57 -0400

    Update controller
    Update controller for new way for drag and drop and EasyAdmin 3+

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -1,25 +1,60 @@
 <?php
-
 namespace Treetop1500\EasyadminDragndropSortBundle\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use ReflectionClass;
-
 
 /**
  * Class DefaultController
  * @package Treetop1500\EasyadminDragndropSortBundle\Controller
  * @author http://github.com/treetop1500
  */
-class DefaultController extends Controller
+class DefaultController extends AbstractController
 {
     /**
      * Resorts an item using it's doctrine sortable property
      *
-     * @Route("/sort/{entityClass}/{id}/{position}", name="easyadmin_dragndrop_sort_sort")
+     * @Route("/sort/{entityClass}", name="easyadmin_dragndrop_sort_sort")
+     * @param String $entityClass
+     * @throws NotFoundHttpException
+     * @throws \ReflectionException
+     * @return JsonResponse
+     *
+     */
+    public function sortAction($entityClass):JsonResponse
+    {
+        $entityClassNameArray = explode('\\', $entityClass);
+        $entityClassName = end($entityClassNameArray);
+        try {
+            $rc = new ReflectionClass($entityClass);
+        } catch (\ReflectionException $error) {
+            throw new \ReflectionException('The class name '. $entityClass .'  cannot be reflected.');
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $datas = json_decode(file_get_contents("php://input"), true) ?: [];
+        foreach ($datas as $data)
+        {
+            $info = explode(',',$data);
+            $e = $em->getRepository($rc->getName())->find($info[0]);
+            if (is_null($e)) {
+                throw new NotFoundHttpException('The entity was not found');
+            }
+            $e->setPosition($info[1]);
+            $em->persist($e);
+            $em->flush();
+        }
+        return $this->json(['success'=>true]);
+    }
+
+    /**
+     * Resorts an item using it's doctrine sortable property
+     *
+     * @Route("/sort/showtable/{entityClass}", name="easyadmin_dragndrop_sort_show_table")
      * @param String $entityClass
      * @param Integer $id
      * @param Integer $position
@@ -28,30 +63,20 @@ class DefaultController extends Controller
      * @return Response
      *
      */
-    public function sortAction($entityClass, $id, $position)
+    public function showTableAction($entityClass)
     {
-
-        $entityClassNameArray = explode("\\", $entityClass);
+        $entityClassNameArray = explode('\\', $entityClass);
         $entityClassName = end($entityClassNameArray);
         try {
             $rc = new ReflectionClass($entityClass);
         } catch (\ReflectionException $error) {
-            throw new \ReflectionException("The class name ". $entityClass ."  cannot be reflected.");
+            throw new \ReflectionException('The class name '. $entityClass .'  cannot be reflected.');
         }
-
-        $em = $this->getDoctrine()->getManager();
-        $e = $em->getRepository($rc->getName())->find($id);
-        if (is_null($e)) {
-            throw new NotFoundHttpException("The entity was not found");
-        }
-        $e->setPosition($position);
-        $em->persist($e);
-        $em->flush();
         return $this->redirectToRoute(
-            "easyadmin",
+            'admin',
             array(
-                "action" => "list",
-                "entity" => $entityClassName,
+                "crudAction" => "index",
+                "crudControllerFqcn" => 'App\\Controller\\Admin\\'.$entityClassName.'CrudController',
                 "sortField" => "position",
                 "sortDirection" => "ASC",
             )
